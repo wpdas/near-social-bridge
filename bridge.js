7f682fd0dc2f9bdd9bb3d03eb7d488d9d98b1510
Wenderson Pires <wendersonpdas@gmail.com>
2023-03-20 16:53:02 -0300

    small implementation with bridge core code

--- bridge.js
+++ bridge.js
@@ -1,170 +1,171 @@
 // Viewer port
-let viewerPort;
+let viewerPort
 
 // Outside of the component state controller
 let state = {
-  externalAppUrl: "",
+  externalAppUrl: '',
   initialPath: null,
-  iframeHeight: 780,
+  iframeHeight: 480,
   sessionStorageClone: {},
   connectMessageSent: false,
-};
+}
 
 // Core Component
 function NearSocialBridgeCore(props) {
-  const [externalAppUrl, setExternalAppUrl] = React.useState(
-    state.externalAppUrl
-  );
-  const [connectMessageSent, setConnectMessageSent] = React.useState(
-    state.connectMessageSent
-  );
-  const [iframeHeight, setIframeHeight] = React.useState(state.iframeHeight);
-  const [sessionStorageClone, setSessionStorageClone] = React.useState(
-    state.sessionStorageClone
-  );
+  const [externalAppUrl, setExternalAppUrl] = React.useState(state.externalAppUrl)
+  const [connectMessageSent, setConnectMessageSent] = React.useState(state.connectMessageSent)
+  const [iframeHeight, setIframeHeight] = React.useState(state.iframeHeight)
+  const [sessionStorageClone, setSessionStorageClone] = React.useState(state.sessionStorageClone)
 
   React.useEffect(() => {
     const handler = (e) => {
       // Set the Viewer port
-      if (!viewerPort && e.data.type === "connect-view") {
-        viewerPort = e.source;
-        setExternalAppUrl(e.data.externalAppUrl);
-        state.externalAppUrl = e.data.externalAppUrl;
-        state.initialPath = e.data.initialPath;
+      if (!viewerPort && e.data.type === 'connect-view') {
+        viewerPort = e.source
+        setExternalAppUrl(e.data.externalAppUrl)
+        setIframeHeight(e.data.initialIframeHeight || 480)
+        state.externalAppUrl = e.data.externalAppUrl
+        state.initialPath = e.data.initialPath
+        state.iframeHeight = e.data.initialIframeHeight || 480
       }
 
       // When get a message from the View
-      if (viewerPort && e.data.from === "view") {
+      if (viewerPort && e.data.from === 'view') {
         // Is it message to the core?
-        if (e.data.type.includes("core:")) {
+        if (e.data.type.includes('core:')) {
           // process eventual message to core here
-          return;
+          return
         }
 
         // Send to external app
-        sendMessage(e.data);
+        sendMessage(e.data)
       }
-    };
+    }
 
-    window.addEventListener("message", handler);
+    window.addEventListener('message', handler)
 
     return () => {
-      window.removeEventListener("message", handler);
-    };
-  }, []);
+      window.removeEventListener('message', handler)
+    }
+  }, [])
 
   const sendMessage = (message) => {
-    var iframe = document.getElementById("myIframe");
-    iframe.contentWindow.postMessage(message, "*");
-  };
+    var iframe = document.getElementById('myIframe')
+    iframe.contentWindow.postMessage(message, '*')
+  }
 
   const sendMessageToView = (message) => {
-    viewerPort.postMessage(message, "*");
-  };
+    viewerPort.postMessage(message, '*')
+  }
 
   // Answer Factory
   const buildAnswer = (requestType, payload) => {
     return {
-      from: "core",
-      type: "answer",
+      from: 'core',
+      type: 'answer',
       requestType,
       payload,
-    };
-  };
+    }
+  }
 
   // Create connection payload
   const createConnectionPayload = () => {
     // Return the connect payload
     return {
-      type: "connect",
+      type: 'connect',
       payload: {
         initialPath: state.initialPath,
       },
       initialPath: state.initialPath,
       created_at: Date.now(),
-    };
-  };
+    }
+  }
 
   const onMessageHandler = (message) => {
     // Internal Request Handler
-    if (message.data.from === "external-app") {
+    if (message.data.from === 'external-app') {
       // Is to the Core
-      if (message.data.type.includes("nsb:")) {
-        internalRequestHandler(message.data);
+      if (message.data.type.includes('nsb:')) {
+        internalRequestHandler(message.data)
       } else {
         // Is to the View
-        sendMessageToView(message.data);
+        sendMessageToView(message.data)
       }
     }
-  };
+  }
 
   // Internal Request handlers
   const internalRequestHandler = (message) => {
     switch (message.type) {
-      case "nsb:session-storage:hydrate-viewer":
-        sessionStorageHydrateViewer(message.type, message.payload);
-        break;
-      case "nsb:session-storage:hydrate-app":
-        sessionStorageHydrateApp(message.type, message.payload);
-        break;
-      case "nsb:navigation:sync-content-height":
-        syncContentHeight(message.type, message.payload);
-        sendMessageToView(message);
-        break;
+      case 'nsb:session-storage:hydrate-viewer':
+        sessionStorageHydrateViewer(message.type, message.payload)
+        break
+      case 'nsb:session-storage:hydrate-app':
+        sessionStorageHydrateApp(message.type, message.payload)
+        break
+      case 'nsb:navigation:sync-content-height':
+        syncContentHeight(message.type, message.payload)
+        sendMessageToView(message)
+        break
     }
-  };
+  }
 
   const sessionStorageHydrateViewer = (requestType, payload) => {
     if (payload) {
-      setSessionStorageClone(payload);
-      state.sessionStorageClone = payload;
+      setSessionStorageClone(payload)
+      state.sessionStorageClone = payload
     }
 
-    const responseBody = buildAnswer(requestType, payload);
-    sendMessage(responseBody);
-  };
+    const responseBody = buildAnswer(requestType, payload)
+    sendMessage(responseBody)
+  }
 
   const sessionStorageHydrateApp = (requestType, payload) => {
-    const responseBody = buildAnswer(requestType, state.sessionStorageClone);
-    sendMessage(responseBody);
-  };
+    const responseBody = buildAnswer(requestType, state.sessionStorageClone)
+    sendMessage(responseBody)
+  }
 
   const syncContentHeight = (requestType, payload) => {
     if (payload.height) {
-      setIframeHeight(payload.height);
-      state.iframeHeight = payload.height;
+      setIframeHeight(payload.height)
+      state.iframeHeight = payload.height
     }
 
-    const responseBody = buildAnswer(requestType);
-    sendMessage(responseBody);
-  };
+    const responseBody = buildAnswer(requestType)
+    sendMessage(responseBody)
+  }
 
   function onLoadHandler(e) {
     // On load iframe
     // On get msg from External App
     if (!connectMessageSent) {
-      setConnectMessageSent(true);
-      state.connectMessageSent = true;
-      window.addEventListener("message", onMessageHandler, false);
+      setConnectMessageSent(true)
+      state.connectMessageSent = true
+      window.addEventListener('message', onMessageHandler, false)
     }
 
-    // Envia mensagem welcome
-    const welcomePayload = createConnectionPayload();
-    sendMessage(welcomePayload);
+    // Send the welcome message (connects with the external app)
+    const welcomePayload = createConnectionPayload()
+    sendMessage(welcomePayload)
+
+    // Wait a bit and send the message again to ensure the app and scripts are loaded and ready
+    setTimeout(() => {
+      sendMessage(welcomePayload)
+    }, 2000)
   }
 
   // Wait for the external app url to render the iframe
-  if (!state.externalAppUrl) return null;
+  if (!state.externalAppUrl) return null
 
-  return React.createElement("iframe", {
-    sandbox: "allow-scripts",
-    id: "myIframe",
+  return React.createElement('iframe', {
+    sandbox: 'allow-scripts',
+    id: 'myIframe',
     src: externalAppUrl,
-    style: { border: "none", width: "100%", height: iframeHeight + "px" },
+    style: { border: 'none', width: '100%', height: iframeHeight + 'px', margin: 0, padding: 0 },
     onLoad: onLoadHandler,
-  });
+  })
 }
 
-const domContainer = document.querySelector("#bridge-root");
-const root = ReactDOM.createRoot(domContainer);
-root.render(React.createElement(NearSocialBridgeCore, {}));
+const domContainer = document.querySelector('#bridge-root')
+const root = ReactDOM.createRoot(domContainer)
+root.render(React.createElement(NearSocialBridgeCore, {}))
