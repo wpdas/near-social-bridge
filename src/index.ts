afa0a0ab5fe7b88dd049b88cf315402525b7c887
Wenderson Pires <wendersonpdas@gmail.com>
2023-04-07 09:50:03 -0300

    fixed issue with `request/index.ts`

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,10 @@
 export * from './bridge'
 export * from './navigation'
-export * from './request'
+
+// for some reason, there's an issue when I try to use index.ts inside "request" folder
+export { default as request } from './request/request'
+export { createMock, setupMock, createMockUser, mockUser } from './request/mock'
+
 export * from './services'
 export * from './session-storage'
 export * from './auth'
