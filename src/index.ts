bdbf45009e670cbfa04ea3c77272bc5b45801bc8
Wenderson Pires <wendersonpdas@gmail.com>
2023-04-11 00:26:23 -0300

    fixed issue where `mockInitialPayload` was not being exported

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ export * from './navigation'
 
 // for some reason, there's an issue when I try to use index.ts inside "request" folder
 export { default as request } from './request/request'
-export { createMock, setupMock, createMockUser, mockUser } from './request/mock'
+export { createMock, setupMock, createMockUser, mockUser, mockInitialPayload } from './request/mock'
 
 export * from './services'
 export * from './session-storage'
---
+++ src/request/index.ts
@@ -0,0 +1,4 @@
+import request from './request'
+import { createMock, setupMock, createMockUser, mockUser, mockInitialPayload } from './mock'
+
+export { request, createMock, setupMock, createMockUser, mockUser, mockInitialPayload }
