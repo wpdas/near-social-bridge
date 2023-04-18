f85963a25304d260609845754b4081915d94aaba
Wenderson Pires <wendersonpdas@gmail.com>
2023-04-18 12:40:17 -0300

    Fixed an issue where the `auth.user` object was defined even when the user was not logged in.

--- src/auth/contexts/AuthProvider.tsx
+++ src/auth/contexts/AuthProvider.tsx
@@ -38,7 +38,7 @@ const AuthProvider: React.FC<{ children: React.ReactNode }> = ({ children }) => 
     const onConnectHandler = () => {
       // Set the initial user info
       const preUserInfo = getConnectionPayload().userInfo
-      if (preUserInfo) {
+      if (preUserInfo?.accountId) {
         setUser(preUserInfo)
       }
 
