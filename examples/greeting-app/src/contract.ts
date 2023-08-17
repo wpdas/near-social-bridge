7c8addbf798dd07dfcbbe1cfb83551e32e5a57b8
Wenderson Pires <wendersonpdas@gmail.com>
2023-08-17 17:17:50 -0300

    removed unused contract id

--- examples/greeting-app/src/contract.ts
+++ examples/greeting-app/src/contract.ts
@@ -1,12 +1,10 @@
-import { Near } from "near-social-bridge";
+import { Near } from 'near-social-bridge'
 
-const CONTRACT_ID = "dev-1692221685438-15421910364142";
-// const CONTRACT_ID = "nearsocialexamples.near";
+const CONTRACT_ID = 'dev-1692221685438-15421910364142'
 
-export const get_greeting = () =>
-  Near.view<string>(CONTRACT_ID, "get_greeting");
+export const get_greeting = () => Near.view<string>(CONTRACT_ID, 'get_greeting')
 
 export const set_greeting = (greeting: string) =>
-  Near.call<{ message: string }>(CONTRACT_ID, "set_greeting", {
+  Near.call<{ message: string }>(CONTRACT_ID, 'set_greeting', {
     message: greeting,
-  });
+  })
