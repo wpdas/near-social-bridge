0dd86df33c589cd13a050c768f39851a5ad3fa53
Wenderson Pires <wendersonpdas@gmail.com>
2023-09-05 18:39:59 -0300

    wip

--- tests/src/services/setTestState.ts
+++ tests/src/services/setTestState.ts
@@ -2,11 +2,7 @@ const setTestState = async (state: boolean) => {
   try {
     // await api().get(`save-test-state?passing=${state === true ? 'true' : 'false'}`)
     await fetch(`/api/save-test-state?passing=${state === true ? 'true' : 'false'}`, {
-      // headers: {
-      // 'Access-Control-Allow-Credentials': true,
-      // ''
-      // },
-      credentials: 'include',
+      // credentials: 'omit',
       mode: 'no-cors',
     })
   } catch (err) {
