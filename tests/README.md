d7ee7f56fbbf93f80a17d40023e4bc32ccb0815f
Wenderson Pires <wendersonpdas@gmail.com>
2023-09-05 22:19:04 -0300

    fix tests api (#10)
    * fix tests api
    
    * README.md Update

---
+++ tests/README.md
@@ -0,0 +1,9 @@
+## About
+
+This is the live test stack application. [**Run Tests Here**](https://near.org/wendersonpires.near/widget/NearSocialBridgeTests)
+
+### FIREBASE API KEY
+
+The Test API is working for production only.
+
+Get the `FIREBASE_API_KEY` inside the `near-social-bridge` project (Firebase). It's also being used with Vercel.
--- tests/src/services/setTestState.ts
+++ tests/src/services/setTestState.ts
@@ -1,14 +1,10 @@
-import { fetch as libFetch } from '@lib'
+import { fetch } from '@lib'
 
 const setTestState = async (state: boolean) => {
   const env = process.env.NODE_ENV
   try {
-    if (env === 'development') {
-      // Vanilla Fetch
-      await fetch(`/api/save-test-state?passing=${state === true ? 'true' : 'false'}`)
-    } else {
-      // Lib Fetch
-      await libFetch(`${window.location.origin}/api/save-test-state?passing=${state === true ? 'true' : 'false'}`)
+    if (env === 'production') {
+      await fetch(`${window.location.origin}/api/save-test-state?passing=${state === true ? 'true' : 'false'}`)
     }
   } catch (err) {
     console.error(err)
